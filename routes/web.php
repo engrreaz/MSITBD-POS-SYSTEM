d85c4d9952626fa9eba20e9e8113ca9b6a838e4b
Reazul Hoque <engrreaz@gmail.com>
2023-06-14 03:33:25 +0600

    Temp

--- routes/web.php
+++ routes/web.php
@@ -22,6 +22,10 @@ Route::get('/dashboard', function () {
     return view('dashboard');
 })->middleware(['auth', 'verified'])->name('dashboard');
 
+Route::get('/dashboard2', function () {
+    return view('dashboard2');
+})->middleware(['auth', 'verified'])->name('dashboard2');
+
 Route::middleware('auth')->group(function () {
     Route::get('/profile', [ProfileController::class, 'edit'])->name('profile.edit');
     Route::patch('/profile', [ProfileController::class, 'update'])->name('profile.update');
